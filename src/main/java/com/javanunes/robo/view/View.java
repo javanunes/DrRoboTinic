5dfa5e723db980fb11541d80fbbdd39e3263507e
javanunes.com@protonmail.com <ricardo@ufo.dugway.gov>
2021-05-13 23:25:27 -0300

    Agora temos a opção de trocarmos a sala para mandar texto usando o comando cd no prompt

--- src/main/java/com/javanunes/robo/view/View.java
+++ src/main/java/com/javanunes/robo/view/View.java
@@ -8,6 +8,16 @@ import java.util.Scanner;
 
 public class View  extends Thread{
     
+    public static void exibirMenuAjuda(){
+        System.out.println("\n\n----- AJUDA DO DRROBOTINIC ------");
+        System.out.println("help   : trás esse menu");
+        System.out.println("cd     : lhe pergunta a sala que você deseja entrar no formato numerico, ex: -192838337");
+        System.out.println("exit   : finaliza esse programa, termina a saida de texto para uma sala");
+        System.out.println("chat   : manda a saida de texto direto para a sala escolhida");
+        System.out.println("");     
+    }
+    
+    
     public static void main(String[] args) {
         try{
           String bannerApresentacao="\n\nMicrocroft Windows Telegram Robotinic [Version 10.0.242469]\n(c) 2015 Microsoft Corporation. All rights reserved.\n";  
@@ -24,32 +34,36 @@ public class View  extends Thread{
               ms.setSaudavel(false);
           }
           
-       
-          
           if(ms.isSaudavel()){
             if(!ms.isSilencioso())  
             System.out.println("Subdindo segundo plano");
-            
             Thread segundoPlano =  new Thread(sp);
             segundoPlano.start();
           }
           
-        
+// ########################################### O MAIN COMEÇA AQUI ##############################################################################################        
           while(ms.isSaudavel()){
              System.out.println(ms.getPrompt()); 
              comando = teclado.nextLine();
              
              switch (comando) {
+                  case "help":
+                    exibirMenuAjuda();
+                    break;
                   case "cd":
-                    
+                    System.out.println("Codigo da sala a mandar: ");
+                    comando = teclado.nextLine();
+                    ms.setSalaID(comando);
+                    ms.setPrompt("C:\\"+comando+"\\>");
+                    comando ="";
                     break;
                   case "chat":
                       while(true){
-                        ms.setPrompt("C:\\sala\\>");
+                        ms.setPrompt("C:\\sendsala\\>");
                         System.out.println(ms.getPrompt());
                         comando = teclado.nextLine();
                         if(comando.equals("exit") || comando.equals("sair") || comando.equals("quit")){
-                            ms.setPrompt(ms.getPrompt());
+                            ms.setPrompt("C:\\Windows\\>");
                             break;
                         }
                         ms.mandarMensagemPraSalaToscamente(comando);
@@ -60,14 +74,12 @@ public class View  extends Thread{
                     break;
                   default:
 
-             }
-          } 
+             }// fim switch
+          } // fim while 
         }
         catch(Exception e){
-            System.out.println("ERRO COMPLEXO TREE:"+e);    
+            System.out.println("O DrRobotinic encontrou uma doença e precisa parar :"+e); 
         }
-        
-        
     } // fim main
     
 }// fim View
