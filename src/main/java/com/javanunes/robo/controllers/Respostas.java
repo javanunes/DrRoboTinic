b99219f568eb6b30649f31c5b0cd7dfd943ea492
javanunes.com@protonmail.com <ricardo@ufo.dugway.gov>
2021-05-16 00:43:18 -0300

    Agora o sistema le de um arquivo respostas pré prontas , alem disso ele pode salvar as suas respostas enquanto você responde usando o comando save, alem disso existe um metodo beta bara banir usuarios do grupo

--- src/main/java/com/javanunes/robo/controllers/Respostas.java
+++ src/main/java/com/javanunes/robo/controllers/Respostas.java
@@ -5,19 +5,80 @@
  */
 package com.javanunes.robo.controllers;
 
+import java.util.Map;
+
 /**
  *
  * @author ricardo
  */
 public class Respostas {
     Mensagens ms = new Mensagens();
+    Arquivos av = new Arquivos();
+    String respostaPronta = "";
+    Map<String,String> respostas,respostasSemNulos = null;
+    
+    
+    public void respondeAutomatico(String pergunta){
+         try{
+          // Aqui se carrega respostas automaticas
+          
+          if(!Mensagens.respostaVeioDoArquivoMemoria)
+             respostas = av.lerRespostasProntasArquivo();
+          
+          if(respostas != null){
+              Mensagens.respostaVeioDoArquivoMemoria = true;
+              respostasSemNulos = respostas;
+          }    
+          
+          if(respostasSemNulos != null ){ 
+              respostaPronta = respostasSemNulos.get(pergunta);      
+              if(!respostaPronta.isBlank() || !respostaPronta.isEmpty()){
+                  ms.mandarMensagemPraSalaToscamente(respostaPronta);
+                  Mensagens.respostaVeioDoArquivoMemoria=true;
+                  Mensagens.respostaManual = false;
+                  limpa();
+              }
+           }
+         }  
+         catch(Exception e){
+             if(!ms.isSilencioso()){
+                System.out.println("Não tenho resposta automática para essa pergunta !");
+             }
+         }
+    }
+    
     
     public void responder(String pergunta){
-              
+         
+        
+        
+          
+          respondeAutomatico(pergunta);
+          
+          // Coloque AQUI as suas perguntas e respostas ################################################
           if(pergunta.contains("Wie gehts")){
               ms.mandarMensagemPraSalaToscamente("Mir geht es gut");
+              Mensagens.respostaManual = false;
               limpa();
           }
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
         
     }
     
